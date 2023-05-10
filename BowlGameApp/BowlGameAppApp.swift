e8f86003fd897c0901a6c001a6aea5d7978d11e8
tylerscott2782 <tylerscott2782@gmail.com>
2023-05-09 20:17:11 -0500

    Initial Commit

---
+++ BowlGameApp/BowlGameAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BowlGameAppApp.swift
+//  BowlGameApp
+//
+//  Created by Tyler Waldo on 5/9/23.
+//
+
+import SwiftUI
+
+@main
+struct BowlGameAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
